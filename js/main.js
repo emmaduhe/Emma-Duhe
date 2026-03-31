97653eb6baa4db9910daef3242b2be13e730f9f5
emmaduhe <emmacduhe@gmail.com>
2026-03-31 10:19:58 -0500

    frog

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // JS scripts placed here
+gsap.to("#F-rightarm", {
+  rotation: 20,              // how far it swings
+  transformOrigin: "top center", // pivot point of rotation
+  duration: 0.6,             // speed of one swing
+  repeat: -1,                // loop forever
+  yoyo: true,                // swing back and forth
+  ease: "power1.inOut"       // smooth movement
+});
