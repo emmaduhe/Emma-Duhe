faac98988088daff8440443588f551dddf530046
emmaduhe <emmacduhe@gmail.com>
2026-03-31 10:38:39 -0500

    animation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 // JS scripts placed here
+// Make the right arm wave
 gsap.to("#F-rightarm", {
-  rotation: 20,              // how far it swings
-  transformOrigin: "top center", // pivot point of rotation
-  duration: 0.6,             // speed of one swing
-  repeat: -1,                // loop forever
-  yoyo: true,                // swing back and forth
-  ease: "power1.inOut"       // smooth movement
+  rotation: 20,                 // how far it swings
+  transformOrigin: "top center", // pivot point of the arm
+  duration: 0.6,                // speed of one swing
+  repeat: -1,                   // loop forever
+  yoyo: true,                   // swing back and forth
+  ease: "power1.inOut"          // smooth motion
 });
